5e8ff14557e34c9c0bc1221ad939bb9eac4261f1
truth <truth.h406@gmail.com>
2021-12-19 20:44:50 +0900

    환경설정, 첫페이지 생성

---
+++ src/pages/wine.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const WinePage: NextPage = () => {
+    return(
+        <div>
+            <h1>Wine</h1>
+        </div>
+    )
+}
+
+export default WinePage;
+//Typescript를 쓰는 가장 기본적인 템플릿.
